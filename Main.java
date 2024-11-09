b0c7805671642c27679cc1b04a1dda1d58a62b60
Prem Shandilya <you@example.com>
2024-11-09 14:34:16 +0530

    added a new code from gfg, not correct still need to improve the code. Fastly Coded.

---
+++ Main.java
@@ -0,0 +1,31 @@
+class Main{
+    public static void main(String[] args) {
+        int n = arr.length;
+        int[] ans = new int[A]
+        int index = n-1;
+        for(int i=n-1; i>=0; i--){
+            while(!st.isEmpty() && arr[i] >=st.peek())
+            {
+                st.pop();
+            
+            }
+            if(st.isEmpty()){
+                ans[i]  == -1;
+            }
+            else{
+                ans[i]  = -1;
+            }
+            else{
+                ans[i] = st.peek();
+            }
+            st.push(arr[i]);
+        }
+        ArrayList<integer> st=newArrayList<>();
+        for(int i =0; i<n ; i++){
+            al.add(ans[i]);
+        }
+        return al;
+    }
+
+
+}
